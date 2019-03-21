b1b90d52f26b39ea6c7930eb6f3337a6825e82d0
w.h.sinn <w.h.sinn@gmail.com>
2019-03-21 02:24:33 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -53,7 +53,7 @@ def for_iterator(integer)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   phrase = "Welcome to Flatiron School's Web Development Course!"
-    puts "#{phrase}"
+    puts {#phrase}
 
   end
 end
